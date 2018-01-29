b23184b8b2675b09b849e17969bcb11ee204b863
Meredith <mbroadway10@yahoo.com>
2018-01-29 16:08:54 -0600

    update student_scores.R

--- student_scores.R
+++ student_scores.R
@@ -1,4 +1,3 @@
-## app.R ##
 library(shiny)
 library(magrittr)
 library(ggplot2)
@@ -8,38 +7,40 @@ satGPA = satGPA
 satGPA$sex = gsub("1", "Male", satGPA$sex) 
 satGPA$sex = gsub("2", "Female", satGPA$sex)
 
-# User interface ----
 ui <- fluidPage(
+  titlePanel("SAT App"),
   
-  titlePanel("Student SAT and GPA Results"),
-  sidebarPanel(
-    # Select Y Axis 
-    selectInput('variable1', 'Y Axis', choices = c("SAT Verbal" = "SATV",
-                                                   "SAT Math" = "SATM",
-                                                   "SAT Sum" = "SATSum",
-                                                   "High School GPA" = "HSGPA",
-                                                   "First Year University GPA" = "FYGPA")),
-    # Select X Axis 
-    selectInput('variable2', 'X Axis', choices = c("SAT Verbal" = "SATV",
-                                                   "SAT Math" = "SATM",
-                                                   "SAT Sum" = "SATSum",
-                                                   "High School GPA" = "HSGPA",
-                                                 "First Year University GPA" = "FYGPA")),
-   
-    # Choose Model to fit  
-    selectInput("model",                       
-                "Model Type",                  
-                choices = c("None" = "none",   
-                            "Linear" = "lm",
-                            "Smooth" = "smooth"))
-  ),
-  mainPanel(
-    plotOutput('plot_data')
+  sidebarLayout(
+    sidebarPanel(
+  
+      selectInput('variable1', 'Y Axis', choices = c("SAT Verbal" = "SATV",
+                                                     "SAT Math" = "SATM",
+                                                     "SAT Sum" = "SATSum",
+                                                     "High School GPA" = "HSGPA",
+                                                     "First Year University GPA" = "FYGPA")),
+      # Select X Axis 
+      selectInput('variable2', 'X Axis', choices = c("SAT Verbal" = "SATV",
+                                                     "SAT Math" = "SATM",
+                                                     "SAT Sum" = "SATSum",
+                                                     "High School GPA" = "HSGPA",
+                                                     "First Year University GPA" = "FYGPA")),
+      
+      # Choose Model to fit  
+      selectInput("model",                       
+                  "Model Type",                  
+                  choices = c("None" = "none",   
+                              "Linear" = "lm",
+                              "Smooth" = "smooth"))
+      ),
+    
+    mainPanel(
+      plotOutput("plot_data")
+    )
   )
-)
+  )
 
-# Server ----
-server <- function(input,output,session){
+# Define server logic ----
+server <- function(input, output) {
   
   # Output plot 
   output$plot_data<-renderPlot({
@@ -47,8 +48,8 @@ server <- function(input,output,session){
     x_axis <- input$variable2
     gg <- ggplot(satGPA, aes_string(x = x_axis, y = y_axis, color = "sex")) 
     gg <- gg + geom_point() 
-  
-     # Display Model Fit (Line through data) 
+    
+    # Display Model Fit (Line through data) 
     ## Linear Model Fit
     if (input$model == "lm") {
       gg <- gg + geom_smooth(method = "lm")
@@ -63,4 +64,5 @@ server <- function(input,output,session){
     gg
   })
 }
-shinyApp(ui, server)
+# Run the app ----
+shinyApp(ui = ui, server = server)
